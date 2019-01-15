7e3f4b1b8bc418a10eae422b60dcfca326337232
Ross Nelson <ross.nelson@gmail.com>
2019-01-14 20:25:58 -0600

    notes

--- main.js
+++ main.js
@@ -35,6 +35,54 @@ let mooIncX = x => {
 
         let weekend = newDate.getDay() == 0 || newDate.getDay() == 6;
         let holiday = holidays.indexOf(newDate.toLocaleDateString(locale)) != -1;
+
+        /*
+TODO: Redo this whole thing. I did it wrong.
+
+WEEKENDS
+===============================================
+Fall on Saturday => Fri
+Fall on Sunday => Mon
+Fall on Fri/Sat of Fri-Mon weekend => Thu
+Fall on Sun/Mon of Fri-Mon weekend => Tue
+Fall on Sat/Sun of Sat-Tue weekend => Fri
+Fall on Mon/Tue of Sat-Tue weekend => Wed
+Fall on Fri/Sat of Fri-Sun weekend => Thu
+Fall on Sun of Fri-Sun weekend = Mon
+Fall on Sat of Sat-Mon weekend => Fri
+Fall on Sun/Mon of Sat-Mon weekend => Tue
+
+HOLIDAYS
+===============================================
+Fall on Monday => ?
+Fall on Tuesday => ?
+Fall on Wednesday => ?
+Fall on Thursday => ?
+Fall on Friday => ?
+
+WEEKENDS + HOLIDAYS
+===============================================
+Fall on Saturday with holiday Friday => ?
+Fall on Saturday with holiday Monday => ?
+Fall on Sunday with holiday Friday => ?
+Fall on Sunday with holiday Monday => ?
+Fall on Fri/Sat of Fri-Mon weekend with holiday Friday => ?
+Fall on Fri/Sat of Fri-Mon weekend with holiday Monday => ?
+Fall on Sun/Mon of Fri-Mon weekend with holiday Friday => ?
+Fall on Sun/Mon of Fri-Mon weekend with holiday Monday => ?
+Fall on Sat/Sun of Sat-Tue weekend with holiday Friday => ?
+Fall on Sat/Sun of Sat-Tue weekend with holiday Monday => ?
+Fall on Mon/Tue of Sat-Tue weekend with holiday Friday => ?
+Fall on Mon/Tue of Sat-Tue weekend with holiday Monday  => ?
+Fall on Fri/Sat of Fri-Sun weekend with holiday Friday => ?
+Fall on Fri/Sat of Fri-Sun weekend with holiday Monday => ?
+Fall on Sun of Fri-Sun weekend with holiday Friday = ?
+Fall on Sun of Fri-Sun weekend with holiday Monday = ?
+Fall on Sat of Sat-Mon weekend with holiday Friday => ?
+Fall on Sat of Sat-Mon weekend with holiday Monday => ?
+Fall on Sun/Mon of Sat-Mon weekend with holiday Friday => ?
+Fall on Sun/Mon of Sat-Mon weekend with holiday Monday => ?
+        */
         
         if (weekend) valid = false;
         if (holiday) valid = false;
